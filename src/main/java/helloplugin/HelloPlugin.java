33070f28203885fe137ab83f6ca70a36c16bb13f
wzx1996 <wzx1996@gmail.com>
2014-06-27 12:19:29 +0800

    Loaded!

--- src/main/java/helloplugin/HelloPlugin.java
+++ src/main/java/helloplugin/HelloPlugin.java
@@ -3,5 +3,13 @@ package helloplugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class HelloPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+    	getLogger().info("Hello bukkit by flamerat!");
+    }
  
+    @Override
+    public void onDisable() {
+        getLogger().info("Bye by flamerat!");
+    }
 }
