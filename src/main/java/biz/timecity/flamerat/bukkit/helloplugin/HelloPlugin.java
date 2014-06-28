5d9ea13082da9a064541df83db26f7909a1d7ebb
wzx1996 <wzx1996@gmail.com>
2014-06-28 12:33:09 +0800

    Add a test command

--- src/main/java/biz/timecity/flamerat/bukkit/helloplugin/HelloPlugin.java
+++ src/main/java/biz/timecity/flamerat/bukkit/helloplugin/HelloPlugin.java
@@ -1,6 +1,10 @@
 package biz.timecity.flamerat.bukkit.helloplugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+
 
 public final class HelloPlugin extends JavaPlugin {
     @Override
@@ -11,5 +15,17 @@ public final class HelloPlugin extends JavaPlugin {
     @Override
     public void onDisable() {
         getLogger().info("Bye by flamerat!");
+    }
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    	if (cmd.getName().equalsIgnoreCase("testcommand")) { 
+    		sender.sendMessage(sender.getName()+", you have issued "+cmd.getName()+" command.");
+    		if (sender.isOp()) sender.sendMessage("You are OP");
+    		else sender.sendMessage("You are not OP");
+    		return true;
+    	} //If this has happened the function will return true. 
+            // If this hasn't happened the value of false will be returned.
+    	return false; 
     }
 }
